0f60ea57f1897ed7c079ae29dc6408fed0ad804c
axiref <axiref@mail.co.uk>
2019-12-05 16:48:34 +0800

    添加命令
    Signed-off-by: axiref <axiref@mail.co.uk>

--- bot.py
+++ bot.py
@@ -3,25 +3,31 @@ import telebot
 from config import TOKEN
 import wikipedia
 from urllib.parse import unquote
+regexp = r'^\[\[(.+?)\]\]|\{\{(.+?)\}\}$'
 
 bot = telebot.TeleBot(TOKEN)
 wikipedia.set_lang("zh")
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, '使用[[]]或{{}}提供到中文維基百科的連結')
+    if (message.chat.type == 'private'):
+        bot.send_message(message.chat.id, '使用[[]]或{{}}提供到中文維基百科的連結')
 
-@bot.message_handler()
-def echo(message):
-    m = re.match("\[\[(.*?)\]\]|\{\{(.*?)\}\}", message.text)
-    if (m is not None):
-        if (m[1] or m[2]):
-            keyword = m[1] or m[2]
-            try:
-                result = wikipedia.page(keyword)
-                bot.reply_to(message, "[%s](%s)" % (unquote(result.url), result.url), parse_mode="markdown")
-            except wikipedia.exceptions.PageError:
-                bot.reply_to(message, '没找到结果')
+@bot.message_handler(commands=['version'])
+def help(message):
+    if (message.chat.type == 'private'):
+        bot.reply_to(message, "https://github.com/axiref/groupwiki")
+
+@bot.message_handler(regexp=regexp)
+def search_keyword(message):
+    m = re.search(regexp, message.text)
+    keyword = m[1] or m[2]
+    if not keyword: return
+    try:
+        result = wikipedia.page(keyword)
+        bot.reply_to(message, "[%s](%s)" % (unquote(result.url), result.url), parse_mode="markdown")
+    except wikipedia.exceptions.PageError:
+        bot.reply_to(message, '没找到结果')
 
 if __name__ == '__main__':
     print('Service is running...')
